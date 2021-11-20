ae23e230530b0cc02c7d69cc18a4b78f33bc23a2
simon <simon.m.binyamin@gmail.com>
2021-11-20 11:30:53 +0100

    first commit

---
+++ Controllers/ScriptController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using AutoCompleteAPI.Models;
+using AutoCompleteAPI.Environment;
+
+namespace AutoCompleteAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ScriptController : ControllerBase
+{
+    private HttpClient client;
+    private Secret secret;
+    public ScriptController()
+    {
+        client = new HttpClient();
+        secret = new Secret();
+    }
+    
+    [Route("Create/{index}/{id}")]
+    public async Task<string> Create(string index,string id)
+    {      
+        string name = Request.Query["name"];
+
+        using (var _client = new HttpClient())
+        {
+
+            JObject jsonObject = new JObject
+            {
+                [index] = new JObject
+                {
+                    ["Id"] = id,
+                    ["Name"] = name
+                }
+            };
+
+            HttpContent content = new StringContent(jsonObject.ToString(), Encoding.UTF8, "application/json");
+
+            var result = await _client.PatchAsync(secret.SCRIPT_LIST+".json?auth=" + secret.API_KEY, content);
+
+           return await result.Content.ReadAsStringAsync();
+        }
+    }
+    
+    [Route("Delete/{index}")]
+    public async Task<string> Delete(string index)
+    {
+
+        using (var _client = new HttpClient())
+        {
+            var result = await _client.DeleteAsync(secret.SCRIPT_LIST +"/"+ index + ".json?auth=" + secret.API_KEY);
+            
+            return await result.Content.ReadAsStringAsync();
+        }
+    }
+    
+    [HttpGet]
+    [Route("List")]
+    public async Task<Item[]?> List()
+    {
+        string resultContent = "";
+
+        HttpResponseMessage response = await client.GetAsync(secret.SCRIPT + ".json");
+        resultContent = await response.Content.ReadAsStringAsync();
+        var json = JObject.Parse(resultContent);
+        var feat = json?["lst"] ?? "";
+        return feat.ToObject<Item[]>();
+    }
+}
